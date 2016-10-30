9872a2df675560b5c9a32dad65b0ca367652a7a5
Gilberth Molina <gmolinac@outlook.com>
2016-10-30 00:58:16 -0600

    Se eliminan todos los archivos de todas las capas para renombrarlos
    Se eliminan todos los archivos de todas las capas para renombrarlos

--- Negocio/Procesos/PersonasCN.js
+++
@@ -1,5 +0,0 @@
-/**
- * Sistema para la Iglesia Manantiales de Vida Eterna
- * Desarrollado por: Gilberth Molina
- * Date: 26/10/16
- */
--- UI/Procesos/Grupos.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Sistema para la Iglesia Manantiales de Vida Eterna
- * Desarrollado por: Gilberth Molina
- * Date: 26/10/16
- */
