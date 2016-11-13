e5bd39279605b6738a1cd757d4ba09be7bd989f4
Gilberth Molina <gmolinac@outlook.com>
2016-11-12 23:23:52 -0600

    Se cambia el menú de las acciones masivas de los grupos.
    Se cambia el menú de las acciones masivas de los grupos.

--- UI/Procesos/grupos.php
+++ UI/Procesos/grupos.php
@@ -145,40 +145,33 @@ $utilitarios = new UtilitariosProcesos();
                         <hr>
                         <div class="row">
                             <div class="col-ws-12">
-                                <div data-role="collapsible" data-theme="b" data-content-theme="a">
-                                    <h2>¿Que desea realizar?</h2>
-                                    <div class="row">
+                                <div data-role="collapsible" data-theme="b" data-content-theme="a" data-inset="true">
+                                    <h2>Contactar personas</h2>
+                                    <fieldset data-role="controlgroup" data-type="horizontal" data-mini="true" class="text-right" style="margin-bottom: 0px" onchange="GruposCargarPersonasGrupoAccionesSeleccion()">
+                                        <input type="radio" name="filtroGruposContacto" id="sms" value="S" checked="checked">
+                                        <label for="sms">SMS</label>
+                                        <input type="radio" name="filtroGruposContacto" id="correo" value="C">
+                                        <label for="correo">Correo</label>
+                                    </fieldset>
+                                    <fieldset data-role="controlgroup" data-type="horizontal" data-mini="true" class="text-right" style="margin-bottom: -10px" onchange="GruposCargarPersonasGrupoAccionesSeleccion()">
+                                        <input type="radio" name="filtroGruposTipoIntegrante" id="lideres" value="S" checked="checked">
+                                        <label for="lideres">Líderes</label>
+                                        <input type="radio" name="filtroGruposTipoIntegrante" id="participantes" value="N">
+                                        <label for="participantes">Participantes</label>
+                                        <input type="radio" name="filtroGruposTipoIntegrante" id="todos" value="">
+                                        <label for="todos">Todos</label>
+                                    </fieldset>
+                                    <br>
+                                    <form class="ui-filterable" style="margin-bottom: -20px">
+                                        <input id="filtroSeleccion" data-type="search" placeholder="Filtrar personas">
+                                    </form>
+                                    <div id="GruposAccionesSeleccion">
+                                        <!-- Aquí se insertan los datos dinámicamente -->
+                                    </div>
+                                    <div class="row text-center">
                                         <div class="col-ws-12">
-                                            <div data-role="collapsible" data-theme="b" data-content-theme="a" data-inset="true">
-                                                <h2>Contactar personas</h2>
-                                                <fieldset data-role="controlgroup" data-type="horizontal" data-mini="true" class="text-right" style="margin-bottom: 0px" onchange="GruposCargarPersonasGrupoAccionesSeleccion()">
-                                                    <input type="radio" name="filtroGruposContacto" id="sms" value="S" checked="checked">
-                                                    <label for="sms">SMS</label>
-                                                    <input type="radio" name="filtroGruposContacto" id="correo" value="C">
-                                                    <label for="correo">Correo</label>
-                                                </fieldset>
-                                                <fieldset data-role="controlgroup" data-type="horizontal" data-mini="true" class="text-right" style="margin-bottom: -10px" onchange="GruposCargarPersonasGrupoAccionesSeleccion()">
-                                                    <input type="radio" name="filtroGruposTipoIntegrante" id="lideres" value="S" checked="checked">
-                                                    <label for="lideres">Líderes</label>
-                                                    <input type="radio" name="filtroGruposTipoIntegrante" id="participantes" value="N">
-                                                    <label for="participantes">Participantes</label>
-                                                    <input type="radio" name="filtroGruposTipoIntegrante" id="todos" value="">
-                                                    <label for="todos">Todos</label>
-                                                </fieldset>
-                                                <br>
-                                                <form class="ui-filterable" style="margin-bottom: -20px">
-                                                    <input id="filtroSeleccion" data-type="search" placeholder="Filtrar personas">
-                                                </form>
-                                                <div id="GruposAccionesSeleccion">
-                                                    <!-- Aquí se insertan los datos dinámicamente -->
-                                                </div>
-                                                <div class="row text-center">
-                                                    <div class="col-ws-12">
-                                                        <a href="sms:" class="ui-shadow ui-btn ui-corner-all ui-btn-b ui-icon-comment ui-btn-icon-left ui-btn-inline ui-mini" id="btnGruposEnviarSMS" onclick="GruposBtnEnviarSMS()">Enviar SMS</a>
-                                                        <a href="mailto:" class="ui-shadow ui-btn ui-corner-all ui-btn-b ui-icon-mail ui-btn-icon-left ui-btn-inline ui-mini" id="btnGruposEnviarCorreo" onclick="GruposBtnEnviarCorreo()">Enviar Correo</a>
-                                                    </div>
-                                                </div>
-                                            </div>
+                                            <a href="sms:" class="ui-shadow ui-btn ui-corner-all ui-btn-b ui-icon-comment ui-btn-icon-left ui-btn-inline ui-mini" id="btnGruposEnviarSMS" onclick="GruposBtnEnviarSMS()">Enviar SMS</a>
+                                            <a href="mailto:" class="ui-shadow ui-btn ui-corner-all ui-btn-b ui-icon-mail ui-btn-icon-left ui-btn-inline ui-mini" id="btnGruposEnviarCorreo" onclick="GruposBtnEnviarCorreo()">Enviar Correo</a>
                                         </div>
                                     </div>
                                 </div>
